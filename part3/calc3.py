ca79a9acc1bb7a20b182a1d64aa5164db4039aff
Guilherme Prokisch <guilherme.prokisch@gmail.com>
2019-02-08 16:19:12 +0600

    Part3

---
+++ part3/calc3.py
@@ -0,0 +1,133 @@
+# Token types
+# EOF( end-of-file) token is used to indicate that 
+# there is no more input left for lexial analysis
+INTEGER, PLUS, MINUS, EOF = 'INTEGER', 'PLUS', 'MINUS', 'EOF'
+MULT, DIVISION = 'MULT', 'DIVISION'
+
+
+class Token(object):
+    def __init__(self, type, value):
+        self.type = type
+        self.value = value
+    
+    def __str__(self):
+        return 'Token({type}, {value})'.format(
+            type=self.type,
+            value=repr(self.value)
+        )
+    
+    def __repr__(self):
+        return self.__str__()
+
+class Interpreter(object):
+    def __init__(self, text):
+        self.text = text
+        self.pos = 0
+        self.current_token = None
+        self.current_char = self.text[self.pos]
+
+    def error(self):
+        raise Exception('Error parsing input')
+    
+    def advance(self):
+        self.pos +=1
+        if self.pos > len(self.text) - 1:
+            self.current_char = None
+        else:
+            self.current_char = self.text[self.pos]
+    
+    def skip_whitespace(self):
+        while self.current_char is not None and self.current_char.isspace():
+            self.advance()
+    
+    def integer(self):
+        result = ''
+        while self.current_char is not None and self.current_char.isdigit():
+            result += self.current_char
+            self.advance()
+        return int(result)
+    
+    def get_next_token(self):
+
+        while self.current_char is not None:
+
+            if self.current_char.isspace():
+                self.skip_whitespace()
+                continue
+            
+            if self.current_char.isdigit():
+                return Token(INTEGER, self.integer())
+            
+            if self.current_char == '+':
+                self.advance()
+                return Token(PLUS, '+')
+            
+            if self.current_char == '/':
+                self.advance()
+                return Token(DIVISION, '-')
+
+            if self.current_char == '*':
+                self.advance()
+                return Token(MULT, '*')
+            
+            if self.current_char == '-':
+                self.advance()
+                return Token(MINUS, '-')
+            
+            self.error()
+
+        return Token(EOF, None)
+    
+    def eat(self, token_type):
+        if self.current_token.type == token_type:
+            self.current_token = self.get_next_token()
+        else:
+            self.error()
+    
+    def term(self):
+        token = self.current_token
+        self.eat(INTEGER)
+        return token.value
+
+
+
+    def expr(self):
+
+        self.current_token = self.get_next_token()
+
+        result = self.term()
+
+        while self.current_token.type in (PLUS, MINUS):
+            token = self.current_token
+            if token.type == PLUS:
+                self.eat(PLUS)
+                result = result + self.term()
+            elif token.type == MINUS:
+                self.eat(MINUS)
+                result = result - self.term()
+        
+        while self.current_token.type in (MULT, DIVISION):
+            token = self.current_token
+            if token.type == MULT:
+                self.eat(MULT)
+                result = result * self.term()
+            elif token.type == DIVISION:
+                self.eat(DIVISION)
+                result = result / float(self.term())
+
+        return result
+
+def main():
+    while True:
+        try:
+            text = raw_input('calc> ')
+        except EOFError:
+                break
+        if not text:
+            continue
+        interpreter = Interpreter(text)
+        result = interpreter.expr()
+        print(result)
+
+if __name__ == '__main__':
+    main()
